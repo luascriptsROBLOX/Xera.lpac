436e35bd8acdb0e6696104029146508dc724574b
luascriptsROBLOX <borahkokil35@gmail.com>
2025-01-19 11:33:10 +0530

    Create Loader.lua

---
+++ Src/Loader.lua
@@ -0,0 +1,21 @@
+local flu = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+
+flu:Notify({
+    Title = "📢  Please Wait.",
+    Content = "give us a second to authenticate you.",
+    Duration = 5
+})
+
+local succ, err = xpcall(function()
+    loadstring(game:HttpGetAsync(".....", true))()
+
+    print("Loaded with 0 issues.")
+end, function(err)
+    if (string.find(err, "404")) or (string.find(err, "attempt to call a nil value")) then
+        flu:Notify({
+            Title = "xera TMP.",
+            Content = "Your executor doesn't support Xera Api",
+            Duration = 5
+        })
+    end
+end)
